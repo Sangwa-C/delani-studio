78dd34232151709f91b40653256fd5bc62ee105d
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-07-25 20:01:20 +0200

    styled the what we do section, to look better.

--- js/srcipt.js
+++ js/srcipt.js
@@ -11,10 +11,18 @@ $(document).ready(function() {
     $("#development").hide();
     $("#delpt").show();
     });
-    $("#delpt").click(function() {
-      $("#delpt").hide();
-      $("#development").show();
-    });  
+  $("#delpt").click(function() {
+    $("#delpt").hide();
+    $("#development").show();
+    }); 
+  $("#product").click(function() {
+    $("#product").hide();
+    $("#pro").show();
+    });
+  $("#pro").click(function() {
+    $("#pro").hide();
+    $("#product").show();
+    });       
 
 
 });
