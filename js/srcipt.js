7452a30442c8f04c2e2f67d61bd31685f73abb40
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-07-26 15:45:23 +0200

    added twxr to be displayed on the photo.

--- js/srcipt.js
+++ js/srcipt.js
@@ -23,7 +23,54 @@ $(document).ready(function() {
     $("#pro").hide();
     $("#product").show();
     });       
-
+  $("#image1").mouseenter(function(){
+    $("#p1").show();
+  }); 
+  $("#image1").mouseleave(function(){
+    $("#p1").hide();
+  });  
+  $("#image2").mouseenter(function(){
+    $("#p2").show();
+  }); 
+  $("#image2").mouseleave(function(){
+    $("#p2").hide();
+  });
+  $("#image3").mouseenter(function(){
+    $("#p3").show();
+  }); 
+  $("#image3").mouseleave(function(){
+    $("#p3").hide();
+  });  
+  $("#image4").mouseenter(function(){
+    $("#p4").show();
+  }); 
+  $("#image4").mouseleave(function(){
+    $("#p4").hide();
+  });  
+  $("#image5").mouseenter(function(){
+    $("#p5").show();
+  }); 
+  $("#image5").mouseleave(function(){
+    $("#p5").hide();
+  }); 
+  $("#image6").mouseenter(function(){
+    $("#p6").show();
+  }); 
+  $("#image6").mouseleave(function(){
+    $("#p6").hide();
+  });
+  $("#image7").mouseenter(function(){
+    $("#p7").show();
+  }); 
+  $("#image7").mouseleave(function(){
+    $("#p7").hide();
+  });
+  $("#image8").mouseenter(function(){
+    $("#p8").show();
+  }); 
+  $("#image8").mouseleave(function(){
+    $("#p8").hide();
+  });
 
 });
 
